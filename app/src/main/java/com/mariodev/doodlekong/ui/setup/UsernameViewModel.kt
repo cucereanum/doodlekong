456abe5cd180491bb89f9be288c029df4f1ba305
cucereanum <cucereanum@yahoo.com>
2023-06-25 18:45:05 +0300

    Create custom views and improve the viewModels for each ui fragment

---
+++ app/src/main/java/com/mariodev/doodlekong/ui/setup/UsernameViewModel.kt
@@ -0,0 +1,59 @@
+package com.mariodev.doodlekong.ui.setup
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mariodev.doodlekong.data.remote.ws.Room
+import com.mariodev.doodlekong.repository.SetupRepository
+import com.mariodev.doodlekong.util.Constants.MAX_ROOM_NAME_LENGTH
+import com.mariodev.doodlekong.util.Constants.MAX_USERNAME_LENGTH
+import com.mariodev.doodlekong.util.Constants.MIN_ROOM_NAME_LENGTH
+import com.mariodev.doodlekong.util.Constants.MIN_USERNAME_LENGTH
+import com.mariodev.doodlekong.util.DispatcherProvider
+import com.mariodev.doodlekong.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class UsernameViewModel @Inject constructor(
+    private val repository: SetupRepository,
+    private val dispatchers: DispatcherProvider
+) : ViewModel() {
+
+    sealed class SetupEvent {
+        object InputEmptyError : SetupEvent()
+        object InputTooShortError : SetupEvent()
+        object InputTooLongError : SetupEvent()
+
+        data class NavigateToSelectRoomEvent(val username: String): SetupEvent()
+
+
+    }
+
+    private val _setupEvent = MutableSharedFlow<SetupEvent>()
+    val setupEvent: SharedFlow<SetupEvent> = _setupEvent
+
+
+    fun validateUsernameAndNavigateToSelectRoom(username: String) {
+        viewModelScope.launch(dispatchers.main) {
+            val trimmedUsername = username.trim()
+            when {
+                trimmedUsername.isEmpty() -> {
+                    _setupEvent.emit(SetupEvent.InputEmptyError)
+                }
+                trimmedUsername.length < MIN_USERNAME_LENGTH -> {
+                    _setupEvent.emit(SetupEvent.InputTooShortError)
+                }
+                trimmedUsername.length > MAX_USERNAME_LENGTH -> {
+                    _setupEvent.emit(SetupEvent.InputTooLongError)
+                }
+                else -> _setupEvent.emit(SetupEvent.NavigateToSelectRoomEvent(username))
+            }
+        }
+    }
+
+}
