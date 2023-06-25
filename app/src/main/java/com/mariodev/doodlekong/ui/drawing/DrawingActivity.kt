fd38a270192de1327801029c320853b612ddaccc
cucereanum <cucereanum@yahoo.com>
2023-06-25 23:09:01 +0300

    Setup the DrawingView and drawing functionality

--- app/src/main/java/com/mariodev/doodlekong/ui/drawing/DrawingActivity.kt
+++ app/src/main/java/com/mariodev/doodlekong/ui/drawing/DrawingActivity.kt
@@ -1,11 +1,15 @@
 package com.mariodev.doodlekong.ui.drawing
 
+import android.graphics.Color
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
+import com.mariodev.doodlekong.R
 import com.mariodev.doodlekong.databinding.ActivityDrawingBinding
+import com.mariodev.doodlekong.util.Constants
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 
@@ -14,7 +18,7 @@ class DrawingActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDrawingBinding
 
-    private val viewModel : DrawingViewModel by viewModels()
+    private val viewModel: DrawingViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,17 +26,37 @@ class DrawingActivity : AppCompatActivity() {
         setContentView(binding.root)
         subscribeToUiStateUpdates()
 
-        binding.colorGroup.setOnCheckedChangeListener {
-            _, checkedId ->
+        binding.colorGroup.setOnCheckedChangeListener { _, checkedId ->
             viewModel.checkRadioButton(checkedId)
         }
     }
 
+    private fun selectColor(color: Int) {
+        binding.drawingView.setColor(color)
+        binding.drawingView.setThickness(Constants.DEFAULT_PAINT_THICKNESS)
+    }
+
     private fun subscribeToUiStateUpdates() {
         lifecycleScope.launchWhenStarted {
-            viewModel.selectedColorButtonId.collect {
-                id -> binding.colorGroup.check(id)
-
+            viewModel.selectedColorButtonId.collect { id ->
+                binding.colorGroup.check(id)
+                when (id) {
+                    R.id.rbBlack -> selectColor(Color.BLACK)
+                    R.id.rbBlue -> selectColor(Color.BLUE)
+                    R.id.rbGreen -> selectColor(Color.GREEN)
+                    R.id.rbOrange -> selectColor(
+                        ContextCompat.getColor(
+                            this@DrawingActivity,
+                            R.color.orange
+                        )
+                    )
+                    R.id.rbRed -> selectColor(Color.RED)
+                    R.id.rbYellow -> selectColor(Color.YELLOW)
+                    R.id.rbEraser -> {
+                        binding.drawingView.setColor(Color.WHITE)
+                        binding.drawingView.setThickness(40f)
+                    }
+                }
             }
         }
     }
